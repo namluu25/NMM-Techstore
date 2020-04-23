f43ccf22617de8688017f6eb5feb5d0cbcf48a60
Nathan <namluu253@gmail.com>
2020-04-23 09:44:27 +0700

    finished header test #41

---
+++ cypress/integration/01-header.spec.js
@@ -0,0 +1,15 @@
+describe('test header', () => {
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from
+        // failing the test
+        return false
+    })
+    it('go to homepage', () => {
+        cy.visit('http://localhost:8080/nmm/public/shop/homepage');
+    })
+    it('test the header', () => {
+      cy.contains('Home').click()
+      cy.contains('Danh mục').click()
+      cy.contains('Liên hệ').click()
+    })
+  })
